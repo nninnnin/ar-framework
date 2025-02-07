01f05b3be780630235f33c7b6f86b3b37a7cb8df
donggyu <watch@rebel9.co.kr>
2025-02-07 14:39:32 +0900

    프로젝트 생성시의 UX 라이팅 수정

--- web/src/components/Project/ProjectRegister.tsx
+++ web/src/components/Project/ProjectRegister.tsx
@@ -37,7 +37,7 @@ const ProjectRegister = ({
               font-weight: bold;
             `}
           >
-            AR 프로젝트 이름을 입력하세요 :)
+            AR 프로젝트 생성하기
           </p>
 
           <ul
@@ -57,11 +57,31 @@ const ProjectRegister = ({
             `}
           >
             <li>
-              AR 타입은 뱃지로 표시되므로 제목에 포함되지 않아도 괜찮아요.
+              AR 프로젝트 생성을 위해 이름을
+              입력해주세요!
             </li>
-            <li>
-              해당 AR 프로젝트의 거점이나 모델의 특징을 포함시키면 좋아요.
-            </li>
+
+            <ul
+              css={css`
+                margin-top: 0.5em;
+
+                & li {
+                  list-style-type: disc;
+                  margin-bottom: 0.3em;
+                  text-align: left;
+                }
+              `}
+            >
+              <li>
+                선택한 AR 타입은 뱃지로 표시해드립니다.
+                제목에 포함되지 않아도 괜찮아요.
+              </li>
+              <li>
+                나중에 프로젝트를 기억하기 쉽게 해당 AR
+                프로젝트의 거점이나 모델의 특징을
+                포함시키면 좋아요.
+              </li>
+            </ul>
           </ul>
         </div>
 
@@ -73,21 +93,27 @@ const ProjectRegister = ({
           `}
           type="text"
           value={projectName}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setProjectName(e.target.value)
-          }
+          onChange={(
+            e: ChangeEvent<HTMLInputElement>
+          ) => setProjectName(e.target.value)}
           autoFocus
           placeholder="예) 소원의 정원 버블버블"
         />
       </div>
 
       <Dialog.ButtonContainer>
-        <Dialog.Button onClick={onPrevious}>이전으로</Dialog.Button>
+        <Dialog.Button onClick={onPrevious}>
+          이전으로
+        </Dialog.Button>
         <Dialog.Button
           onClick={() => onFinalize(projectName)}
           cssOverlap={css`
-            background-color: ${Boolean(projectName) ? "black" : "white"};
-            color: ${Boolean(projectName) ? "white" : "black"};
+            background-color: ${Boolean(projectName)
+              ? "black"
+              : "white"};
+            color: ${Boolean(projectName)
+              ? "white"
+              : "black"};
           `}
         >
           프로젝트 생성
