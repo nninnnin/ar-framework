2c55a6dd4dc239557b36d8c4096114c4eb432bd7
justindglee <nninnnin7@gmail.com>
2025-03-26 19:05:38 +0900

    `@ar-framework/capturer`: 캡쳐 버튼에 핸들러 추가, 캡쳐기능 초기화

--- packages/capturer/src/index.ts
+++ packages/capturer/src/index.ts
@@ -1,9 +1,13 @@
 import "./styles.css";
+import { Capturer } from "./utils/index.ts";
 
 const captureButton = document.createElement("button");
 
 captureButton.id = "capture-button";
+captureButton.addEventListener("click", () => {
+  const capturer = new Capturer();
+
+  capturer.capture();
+});
 
 document.body.appendChild(captureButton);
-
-console.log(captureButton);
--- packages/capturer/src/utils/index.ts
+++ packages/capturer/src/utils/index.ts
@@ -4,9 +4,79 @@ export class Capturer {
   constructor() {
     const canvas = document.createElement("canvas");
 
+    canvas.id = "capture-canvas";
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
     this.canvas = canvas;
   }
 
-  drawScene() {}
-  drawVideo() {}
+  getScene() {
+    const scene = document.querySelector("a-scene");
+
+    if (!scene) {
+      throw Error(
+        "No a-scene element found in the document"
+      );
+    }
+
+    return scene;
+  }
+
+  getVideo() {
+    const video = document.querySelector("video");
+
+    if (!video) {
+      throw Error(
+        "No video element found in the document"
+      );
+    }
+
+    return video;
+  }
+
+  stopVideo(video) {
+    video.pause();
+  }
+
+  captureScene(scene) {
+    const capturedScene =
+      scene.components.screenshot.getCanvas(
+        "perspective"
+      );
+
+    return capturedScene;
+  }
+
+  drawScene() {
+    const scene = this.getScene();
+    const capturedScene = this.captureScene(scene);
+
+    const ctx = this.canvas.getContext("2d");
+    ctx.drawImage(capturedScene, 0, 0);
+  }
+
+  drawVideo() {
+    const video = this.getVideo();
+    this.stopVideo(video);
+
+    const ctx = this.canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0);
+  }
+
+  capture() {
+    this.drawVideo();
+    this.drawScene();
+
+    this.appendCanvasToBody();
+  }
+
+  appendCanvasToBody() {
+    document.body.appendChild(this.canvas);
+  }
+
+  removeCanvasFromBody() {
+    document.body.removeChild(this.canvas);
+  }
 }
