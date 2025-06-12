009d7acfcc519ebd992f980665664e1545b063e4
justindglee <nninnnin7@gmail.com>
2025-06-12 15:04:59 +0900

    `@ar-framework/web`: 얼굴인식과 이미지 마커 방식 스타일시트 수정

--- web/src/app/templates/constants/templateContents.ts
+++ web/src/app/templates/constants/templateContents.ts
@@ -50,6 +50,8 @@ export const TemplateContents: Record<
   },
   "얼굴인식 AR": {
     headScripts: `
+      <link rel="stylesheet" href="/styles/face-tracking.css" >
+
       <script src="https://aframe.io/releases/1.5.0/aframe.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-face-aframe.prod.js"></script>
     `,
